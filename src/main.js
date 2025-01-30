f99f414e10165530f2a2d5b265a979b09023ae87
Adailton Moura <contatoadailtonmoura@gmail.com>
2025-01-30 02:51:50 -0300

    feature: adiciona o vue icons

--- src/main.js
+++ src/main.js
@@ -6,8 +6,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { BiGithub, BiLinkedin, MdEmailSharp, RiZhihuFill } from 'oh-vue-icons/icons'
+
+addIcons(BiGithub, BiLinkedin, MdEmailSharp, RiZhihuFill);
+
 const app = createApp(App)
 
+app.component("v-icon", OhVueIcon);
 app.use(createPinia())
 app.use(router)
 
